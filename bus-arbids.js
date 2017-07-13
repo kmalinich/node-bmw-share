7f82b45ff3c58d2943fc4beb2cf16b50c2d8cc45
kdm <kennygprs@gmail.com>
2017-07-12 23:56:00 -0400

    Update

--- bus-arbids.js
+++ bus-arbids.js
@@ -2,19 +2,17 @@ const module_name = __filename.slice(__dirname.length + 1, -3).replace('-', '_')
 
 const arbids = {
 	// These are related to E39/E46/E53
-	'0x153' : 'ASC1',  // ABS/ASC/DSC - Speed (E46)
-	'0x1F0' : 'ASC1',  // ABS/ASC/DSC - Wheel speeds (E46)
-	'0x1F3' : '',
-	'0x1F5' : 'ASC1',  // ABS/ASC/DSC - Steering angle sensor (E46)
-	'0x316' : 'DME1',  // DME - RPM (E46)
-	'0x329' : 'DME1',  // DME - Temp (E46)
-	'0x545' : 'DME1',  // DME - CEL/Fuel cons/Overheat/Oil temp/Charging (E46)
-	'0x610' : 'DME1',  // DME - VIN/info (E46)
-	'0x613' : 'DME1',  // DME - Odometer/Running clock (E46)
-	'0x615' : 'DME1',  // DME - A/C request/Outside air temp (E46)
-	'0x77C' : '',
-	'0x77F' : '',
-	'0x7B5' : '',
+	'0x153' : 'ASC1', // Speed/DSC light
+	'0x1F0' : 'ASC1', // Wheel speeds
+	'0x1F3' : 'ASC1', // Transverse acceleration/VSS
+	'0x1F5' : 'ASC1', // Steering angle
+	'0x1F8' : 'ASC1'  // Brake pressure
+	'0x316' : 'DME1', // RPM
+	'0x329' : 'DME1', // Temp/Brake pedal depressed/Throttle position
+	'0x545' : 'DME1', // CEL/Fuel cons/Overheat/Oil temp/Charging/Brake light switch/Cruise control
+	'0x610' : 'DME1', // VIN/info
+	'0x613' : 'DME1', // Odometer/Running clock/Fuel level [0x615 ACK]
+	'0x615' : 'DME1', // A/C request/Outside air temp/Intake air temp/Parking brake/door contacts
 
 	// Below are related to iDrive CON - ripped from E90
 	'0x273' : 'CIC',   // CIC->CON init message
@@ -25,6 +23,11 @@ const arbids = {
 	'0x5E7' : 'CON',   // CON status
 	'0x4F8' : 'KOMBI', // Ignition status
 	'0x202' : 'FRM1',  // FRM backlight dimmer
+
+	// Nobody has ever heard of these..
+	'0x77C' : '',
+	'0x77F' : '',
+	'0x7B5' : '',
 };
 
 module.exports = {
