723d8f01d3dcddbff05828ef861516c5064ddbfd
kdm <kennygprs@gmail.com>
2017-10-01 14:06:36 -0400

    Update

--- update.js
+++ update.js
@@ -15,49 +15,51 @@ const config_transform = {
 
 
 // update('system.host_data.refresh_interval', 15000);
-function update_config(key, value_new) {
+function update_config(key, value_new, verbose = true) {
 	let value_old = object_path.get(config, key);
 
 	if (value_new === value_old) {
 		return false;
 	}
 
-	log.change({
-		src   : module_name,
-		value : 'config.' + key,
-		old   : value_old,
-		new   : value_new,
-	});
+	if (verbose === true) {
+		log.change({
+			src   : module_name,
+			value : 'config.' + key,
+			old   : value_old,
+			new   : value_new,
+		});
+	}
 
 	object_path.set(config, key, value_new);
 
 	// let path_array = key.split('.');
-	// let path_depth = path_array.length;
 	// socket.config_tx(path_array[0]);
 
 	return true;
 }
 
-// update('engine.rpm', 1235);
-function update_status(key, value_new) {
+// update('engine.rpm', 1235, true);
+function update_status(key, value_new, verbose = false) {
 	let value_old = object_path.get(status, key);
 
 	if (value_new === value_old) {
 		return false;
 	}
 
-	log.change({
-		src   : module_name,
-		value : 'status.' + key,
-		old   : value_old,
-		new   : value_new,
-	});
+	if (verbose === true) {
+		log.change({
+			src   : module_name,
+			value : 'status.' + key,
+			old   : value_old,
+			new   : value_new,
+		});
+	}
 
 	object_path.set(status, key, value_new);
 
-	let path_array = key.split('.');
-	// let path_depth = path_array.length;
-	socket.status_tx(path_array[0]);
+	// let path_array = key.split('.');
+	// socket.status_tx(path_array[0]);
 
 	return true;
 }
@@ -66,6 +68,6 @@ function update_status(key, value_new) {
 module.exports = {
 	transform : config_transform,
 
-	config : (key, value_new) => { return update_config(key, value_new); },
-	status : (key, value_new) => { return update_status(key, value_new); },
+	config : (key, value_new, verbose) => { return update_config(key, value_new, verbose); },
+	status : (key, value_new, verbose) => { return update_status(key, value_new, verbose); },
 };
