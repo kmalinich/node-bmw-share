ecdbc32a30bdf4b45c6740040eebd8529ed298d5
kdm <kennygprs@gmail.com>
2018-03-04 20:12:49 -0500

    Update

--- bus-arbids.js
+++ bus-arbids.js
@@ -3,30 +3,69 @@
 // CON1 : iDrive controller
 // NBT1 : NBT_HU (HW07)
 const arbids = {
-	'0x0BF' : 'CON1', // CON1 touchpad
-	'0x264' : 'CON1', // CON1 rotation
-	'0x267' : 'CON1', // CON1 buttons
-	'0x277' : 'CON1', // CON1->NBT1 ACK of init
-	'0x4E7' : 'CON1', // CON1 status
-	'0x5E7' : 'CON1', // CON1 status
+	// Related to iDrive CON1 - ripped from E90/F10/G30
+	'0x202' : 'FEM1',  // Backlight dimmer
 
-	// Below are related to iDrive CON1 - ripped from E90/F10/G30
-	'0x202' : 'FRM1',   // FRM backlight dimmer
 	'0x4F8' : 'KOMBI1', // Ignition status
 
-	// Below are related to NBT1 retrofit
+	// Related to NBT1 retrofit
 	'0x12F' : 'CAS1', // NBT1/ZBE1 wakeup
 	'0x130' : 'CAS1', // Ignition status, key status
+
 	'0x1D6' : 'SZL1', // Steering wheel controls
+
 	'0x26E' : 'CAS1', // Ignition status
 	'0x2FC' : 'CAS1', // Door status
 	'0x380' : 'CAS1', // VIN, info
 	'0x7C3' : 'CAS1', // Keyfob
-	'0x273' : 'NBT1', // NBT1->CON1 init message
+
+
+	'0x0BF' : 'CON1', // Touchpad
+	'0x264' : 'CON1', // Rotation
+	'0x267' : 'CON1', // Buttons
+	'0x277' : 'CON1', // CON1->NBT1 ACK of init
+	'0x4E7' : 'CON1', // Status
+	'0x5E7' : 'CON1', // Status
+
 	'0x000' : 'NBT1',
+	'0x228' : 'NBT1',
+	'0x24F' : 'NBT1',
+	'0x273' : 'NBT1', // NBT1->CON1 init message
+	'0x278' : 'NBT1',
+	'0x27A' : 'NBT1',
+	'0x341' : 'NBT1',
+	'0x348' : 'NBT1',
+	'0x34A' : 'NBT1',
+	'0x34C' : 'NBT1',
+	'0x34E' : 'NBT1',
+	'0x38A' : 'NBT1',
+	'0x38D' : 'NBT1',
+	'0x3CC' : 'NBT1',
+	'0x3F7' : 'NBT1',
+	'0x413' : 'NBT1',
+	'0x415' : 'NBT1',
+	'0x42C' : 'NBT1',
+	'0x436' : 'NBT1',
+	'0x43C' : 'NBT1',
+	'0x43D' : 'NBT1',
+	'0x563' : 'NBT1',
+	'0x5E3' : 'NBT1',
+	'0x663' : 'NBT1',
 
 
-	// These are related to E38/E39/E46/E53
+	// E65, G30, possibly others
+	'0x21A' : '', // Light status
+	'0x1F6' : '', // Turn signals
+	'0x3B6' : '', // Window status LF
+	'0x3B7' : '', // Window status LR
+	'0x3B8' : '', // Window status RF
+	'0x3B9' : '', // Window status RR
+	'0x0FA' : '', // Window commands
+	'0x1E7' : '', // Seat heating
+	'0x1C0' : 'ASC1', // Vehicle speed
+
+
+	// From E38/E39/E46/E53
 	'0x153' : 'ASC1', // Speed, DSC light
 	'0x1F0' : 'ASC1', // Wheel speeds
 	'0x1F3' : 'ASC1', // Transverse acceleration, VSS
