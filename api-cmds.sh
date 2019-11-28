af273faaa369644c4db276156094632dfb2a8978
kdm <kennygprs@gmail.com>
2019-11-27 19:04:10 -0500

    Update

--- api-cmds.sh
+++ api-cmds.sh
@@ -3,6 +3,13 @@
 SCRIPT="$(realpath "${0}")"
 SCRIPT_PATH="$(dirname "${SCRIPT}")"
 
-API_JS="${SCRIPT_PATH}/api.js"
+API_JS="${SCRIPT_PATH}/lib/api.js"
 
-grep -Eo "app\..*.\('.*.'" "${API_JS}" | grep -Ev '\.all\(' | sed -e 's/app\.//g' -e 's/get(/GET\ \ /g' -e 's/post(/POST\ /g'
+tty-table \
+	--options-align=left   \
+	--options-compact=false \
+	--options-borderColor=blue \
+	< <({
+	echo "Method,Command"
+	grep -Eo "app\..*.\('.*.'" "${API_JS}" | grep -Ev '\.all\(' | sed -e 's/app\.//g' -e 's/get(/GET,/g' -e 's/post(/POST,/g' -e "s/'//g" | awk -F ',' '{print $2 "," $1}'
+})
---
+++ index.js
@@ -0,0 +1,23 @@
+import api           from './lib/api.js';
+import bitmask       from './lib/bitmask.js';
+import bus           from './lib/bus.js';
+import hex           from './lib/hex.js';
+import host_data     from './lib/host-data.js';
+import log_output    from './lib/log-output.js';
+import notify        from './lib/notify.js';
+import num           from './lib/num.js';
+import object_format from './lib/object-format.js';
+import update        from './lib/update.js';
+
+export default {
+	api,
+	bitmask,
+	bus,
+	hex,
+	host_data,
+	log_output,
+	notify,
+	num,
+	object_format,
+	update,
+};
